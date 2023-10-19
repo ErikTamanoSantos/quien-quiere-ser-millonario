df94b8aa89d69175597c774431c756f7fe8eccda
polcortes <pcortesgarcia.cf@iesesteveterradas.cat>
2023-10-19 20:13:02 +0200

    Página de inicio

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Qui vol ser milionari?</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <nav id="index-nav">
+            <form action="" method="get">
+                <select name="idioma" id="idioma">
+                    <option value="cat">🏳️‍🌈 Català</option>
+                    <option value="esp">🇪🇸 Español</option>
+                    <option value="eng">🇺🇸 English</option>
+                </select>
+    
+                <input type="submit" value="Canviar" disabled>
+            </form>
+        </nav>
+        <section id="header-title">
+            <h1>Qui vol ser<br>MILIONARI</h1>
+            <a href="game.php">JUGAR</a>
+        </section>
+        <section id="header-scroll">
+            <p>Fes scroll cap abaix per <br>veure les instruccions</p>
+            <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-big-down-filled" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M10 2l-.15 .005a2 2 0 0 0 -1.85 1.995v6.999l-2.586 .001a2 2 0 0 0 -1.414 3.414l6.586 6.586a2 2 0 0 0 2.828 0l6.586 -6.586a2 2 0 0 0 .434 -2.18l-.068 -.145a2 2 0 0 0 -1.78 -1.089l-2.586 -.001v-6.999a2 2 0 0 0 -2 -2h-4z" stroke-width="0" fill="currentColor"></path>
+            </svg>
+        </section>
+    </header>
+    
+    <section>
+        <h1>Com jugar al "Qui vol ser milionari"</h1>
+        <div>
+            <p>En aquest joc hauràs d'afrontar-te a 18 preguntes </p>
+            <img src="" alt="">
+        </div>
+    </section>
+</body>
+</html>
