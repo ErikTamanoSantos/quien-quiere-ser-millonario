b1daa8863ff60b0d50b375ff747437f8bb116ad7
ErikTamano <72156568+ErikTamanoSantos@users.noreply.github.com>
2023-10-24 15:47:14 +0200

    Rename styles.css to style.css

--- game.php
+++ game.php
@@ -8,7 +8,7 @@
     <title>Document</title>
 </head>
 <body>
-    <main class="main-slider">
+    <main>
     <?php 
         $file = fopen("preguntes/catalan_1.txt", "r");
         $question_prefix = "* ";
@@ -76,7 +76,7 @@
 
         function print_page_from_data($questions_array, $selected_indexes, $question_prefix, $correct_answer_prefix, $wrong_answer_prefix) {
             $question = key($questions_array[$selected_indexes[0]]);
-            echo "<div id='question-0' class='question-container slider' question-number='0'>\n";
+            echo "<div id='question-0' class='question-container' question-number='0'>\n";
             echo "<div class='question-header'>\n";
             echo "<h1>".remove_prefix($question, $question_prefix, $correct_answer_prefix, $wrong_answer_prefix)."</h1>\n";
             echo "</div>\n";
@@ -93,7 +93,7 @@
             echo "<div class='message-container'>";
             echo "<div class='message-correct'>";
             echo "<h2>Correcte!</h2>";
-            echo "<div class='arrow-down'>";
+            echo "<div class='arrow-container'>";
             echo '<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-arrow-big-down-filled" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                     <path d="M10 2l-.15 .005a2 2 0 0 0 -1.85 1.995v6.999l-2.586 .001a2 2 0 0 0 -1.414 3.414l6.586 6.586a2 2 0 0 0 2.828 0l6.586 -6.586a2 2 0 0 0 .434 -2.18l-.068 -.145a2 2 0 0 0 -1.78 -1.089l-2.586 -.001v-6.999a2 2 0 0 0 -2 -2h-4z" stroke-width="0" fill="currentColor"></path>
@@ -108,7 +108,7 @@
 
             for ($i = 1; $i < count($selected_indexes); $i++) {
                 $question = key($questions_array[$selected_indexes[$i]]);
-                echo "<div id='question-$i' class='question-container hidden-question slider' question-number='$i'>\n";
+                echo "<div id='question-$i' class='question-container hidden-question' question-number='$i'>\n";
                 echo "<div class='question-header'>\n";
                 echo "<h1>".remove_prefix($question, $question_prefix, $correct_answer_prefix, $wrong_answer_prefix)."</h1>";
                 echo "</div>\n";
